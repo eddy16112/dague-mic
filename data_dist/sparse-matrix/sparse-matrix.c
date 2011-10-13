9b3f1e0c71f4e1fcb50b6ea1875d6ded30ec6d73
Mathieu Faverge <mfaverge@eecs.utk.edu>
2011-10-13 11:43:11 +0200

    Add sparse datatype

---
+++ data_dist/sparse-matrix/sparse-matrix.c
@@ -0,0 +1,128 @@
+/**
+ *
+ * @file sparse-matrix.c
+ *
+ * @author Mathieu Faverge
+ * @date 2011-03-01
+ * @precisions normal z -> c d s
+ *
+ **/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+#include <errno.h>
+#include <stdarg.h>
+#include <stdint.h>
+
+#ifdef HAVE_MPI
+#include <mpi.h>
+#endif /* HAVE_MPI */
+
+#include "dague_config.h"
+#include "dague.h"
+#include "data_dist/sparse-matrix/sparse-matrix.h"
+
+static int sparse_matrix_size_of(enum spmtx_type type)
+{
+    switch ( type ) {
+    case spmtx_RealFloat:
+        return sizeof(float);
+    case spmtx_RealDouble:
+        return sizeof(double);
+    case spmtx_ComplexFloat:
+        return sizeof(Dague_Complex32_t);
+    case spmtx_ComplexDouble:
+        return sizeof(Dague_Complex64_t);
+    default:
+        return sizeof(float);
+    }
+}
+
+uint32_t sparse_matrix_rank_of(struct dague_ddesc *mat, dague_int_t cblknum, dague_int_t bloknum)
+{
+    return 0;
+}
+
+void *sparse_matrix_data_of(struct dague_ddesc *mat, dague_int_t cblknum, dague_int_t bloknum)
+{
+    sparse_matrix_desc_t *spmtx = (sparse_matrix_desc_t*)mat;
+    
+    return spmtx.symbmtx.cblktab[cblknum].cblkptr 
+        + (size_t)(spmtx.typesze) * (size_t)(spmtx.symbmtx.bloktab[bloknum].coefind);
+}
+
+#ifdef DAGUE_PROF_TRACE
+uint32_t sparse_matrix_data_key(struct dague_ddesc *mat, dague_int_t cblknum, dague_int_t bloknum)
+{
+  return (uint32_t)bloknum;
+}
+
+int sparse_matrix_key_to_string(struct dague_ddesc *mat, uint32_t datakey, char *buffer, uint32_t buffer_size)
+{
+    sparse_matrix_desc_t *spmtx = (sparse_matrix_desc_t*)mat;
+    dague_int_t bloknum, cblknum;
+    dague_int_t first, last, middle;
+    
+    first   = 0;
+    last    = spmtx.symbmtx.cblknbr;
+    middle  = (last+first) / 2;
+    bloknum = (dague_int_t)datakey;
+    cblknum = -1;
+
+    while( last - first > 0 ) {
+        if ( bloknum >= spmtx.symbmtx.cblktab[middle].bloknum ) {
+            if ( bloknum < spmtx.symbmtx.cblktab[middle+1].bloknum ) {
+                cblknum = middle;
+                break;
+            }
+            first = middle;
+        }
+        else {
+            last = middle;
+        }
+        middle = (last+first) / 2;
+    }
+
+    res = snprintf(buffer, buffer_size, "(%ld, %ld)", 
+                   (long int)cblknum, (long int)bloknum);
+    if (res < 0)
+    {
+        printf("error in key_to_string for tile (%ld, %ld) key: %u\n", 
+               (long int)cblknum, (long int)bloknum, datakey);
+    }
+    return res;
+}
+#endif
+
+void sparse_matrix_init( sparse_matrix_t *desc, 
+                         enum matrix_type mtype, 
+                         int nodes, int cores, int myrank)
+{
+    /* dague_ddesc structure */
+    desc->super.nodes   = nodes;
+    desc->super.cores   = cores;
+    desc->super.myrank  = myrank;
+    desc->super.rank_of = sparse_matrix_rank_of;
+    desc->super.data_of = sparse_matrix_data_of;
+#ifdef DAGUE_PROF_TRACE
+    desc->super.data_key      = sparse_matrix_data_key;
+    desc->super.key_to_string = sparse_matrix_key_to_string;
+    desc->super.key    = NULL; /* Initialized when the matrix is read */
+    desc->super.keydim = NULL; /* Initialized when the matrix is read */
+#endif /* DAGUE_PROF_TRACE */
+
+    desc->mtype   = mtype;
+    desc->typesze = sparse_matrix_sizeof( mtype );
+
+    DEBUG(("sparse_matrix_init: desc = %p, mtype = %zu, \n"
+           "\tnodes = %u, cores = %u, myrank = %u\n",
+           desc, (size_t) desc->super.mtype, 
+           desc->super.super.nodes, 
+           desc->super.super.cores,
+           desc->super.super.myrank));
+
+}
+
+#endif /* DAGUE_PROF_TRACE */
---
+++ data_dist/sparse-matrix/sparse-matrix.h
@@ -0,0 +1,82 @@
+/**
+ *
+ * @file sparse-matrix.h
+ *
+ * @author Mathieu Faverge
+ * @date 2011-03-01
+ * @precisions normal z -> c d s
+ *
+ **/
+
+#ifndef _SPARSE_MATRIX_H_ 
+#define _SPARSE_MATRIX_H_ 
+
+#include <stdint.h>
+#include "dague_config.h"
+#include "precision.h"
+#include "data_distribution.h"
+
+typedef int64_t dague_int_t;
+
+#define DSPARSE_LLT   1
+#define DSPARSE_LDLT  2 
+#define DSPARSE_LU    3
+
+enum spmtx_type {
+    spmtx_RealFloat     = 0,  /**< float          */
+    spmtx_RealDouble    = 1,  /**< double         */
+    spmtx_ComplexFloat  = 2,  /**< complex float  */
+    spmtx_ComplexDouble = 3,  /**< complex double */
+};
+
+typedef struct dague_symbol_blok {
+    dague_int_t frownum;                 /**< First row index                       */
+    dague_int_t lrownum;                 /**< Last row index (inclusive)            */
+    dague_int_t coefind;                 /**< Index of the first byte of this block */
+} dague_symbol_block_t;
+
+typedef struct dague_symbol_cblk {
+    dague_int_t    fcolnum;              /**< First column index                      */
+    dague_int_t    lcolnum;              /**< Last column index (inclusive)           */
+    dague_int_t    bloknum;              /**< First block in column (diagonal)        */
+    dague_int_t    stride;               /**< Leading dimension of the column data    */
+    void          *cblkptr;              /**< Pointer to the column data (Lower part) */
+    void          *ucblkptr;             /**< Pointer to the column data (Upper part) */
+} dague_symbol_cblk_t;
+
+typedef struct dague_sparse_input_symbol_matrix {
+    int         elemsze;                    /**< Size of an element                */
+    dague_int_t cblknbr;                    /**< Number of column blocks           */
+    dague_int_t bloknbr;                    /**< Number of blocks                  */
+    dague_int_t nodenbr;                    /**< Number of nodes in matrix         */
+    dague_symbol_cblk_t * restrict cblktab; /**< Array of column blocks (size:cblknbr+1,based to 0) */
+    dague_symbol_blok_t * restrict bloktab; /**< Array of blocks (size:bloknbr,based to 0)          */
+} dague_symbol_matrix_t;
+
+typedef struct sparse_matrix_desc_t {
+    dague_ddesc_t         super;
+    enum spmtx_type       mtype;   /* precision of the matrix             */
+    int                   typesze; /* Type size                           */
+    dague_symbol_matrix_t symbmtx; /* Pointer to symbol matrix structure  */
+} sparse_matrix_desc_t;
+
+typedef struct dsp_context_s {
+    int          format;     /* Matrix file format                         */
+    int          factotype;
+    char        *matrixname; /* Filename to get the matrix                 */
+    char        *ordername;  /* Filename where the ordering is stored      */
+    char        *symbname;   /* Filename where the symbol matrix is stored */
+    char        *type;       /* Type of the matrix                         */
+    char        *rhstype;    /* Type of the RHS                            */
+    dague_int_t  n;          /* Number of unknowns/columns/rows            */
+    dague_int_t  nnz;        /* Number of non-zero values in the input matrix */
+    dague_int_t *colptr;     /* Vector of size N+1 storing the starting point of each column in the array rows */
+    dague_int_t *rows;       /* Indices of the rows present in each column */
+    void        *values;     /* Values of the matrix                       */
+    void        *rhs;        /* Right Hand Side                            */ 
+    dague_int_t *permtab;    /* vector of permutation                      */
+    dague_int_t *peritab;    /* vector of inverse permutation              */
+    sparse_matrix_desc_t *symbmtx; /* Pointer to symbol matrix structure */
+} sparse_context_t;
+
+#endif /* _SPARSE_MATRIX_H_ */
