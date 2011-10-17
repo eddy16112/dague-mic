878d268c72a5619d51709b3eeff84ec0bb44fcb5
Mathieu Faverge <mfaverge@eecs.utk.edu>
2011-10-17 18:28:51 +0200

    add forgotten file

---
+++ data_dist/sparse-matrix/pastix_internal/pastix_internal.h
@@ -0,0 +1,31 @@
+#ifndef _DAGUE_PASTIX_INTERNAL_H_
+#define _DAGUE_PASTIX_INTERNAL_H_
+
+#include "dof.h"
+#include "ftgt.h"
+#include "symbol.h"
+#include "csc.h"
+#include "updown.h"
+#include "queue.h"
+#include "bulles.h"
+#include "solver.h"
+#include "assembly.h"
+#include "param_blend.h"
+#include "order.h"
+#include "fax.h"
+#include "kass.h"
+#include "blend.h"
+#include "solverRealloc.h"
+#include "sopalin_thread.h"
+#include "stack.h"
+#include "sopalin3d.h"
+#include "sopalin_init.h"
+#include "sopalin_option.h"
+#include "csc_intern_updown.h"
+#include "csc_intern_build.h"
+#include "coefinit.h"
+#include "out.h"
+#include <pastix.h>
+#include "pastixstr.h"
+
+#endif /* _DAGUE_PASTIX_INTERNAL_H_ */
