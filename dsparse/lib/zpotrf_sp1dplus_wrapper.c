b0664892ba3d8e772cc693d90b68f1da02868e74
Mathieu Faverge <mfaverge@eecs.utk.edu>
2011-10-19 15:32:38 +0200

    Add zpotrf wrapper

---
+++ dsparse/lib/zpotrf_sp1dplus_wrapper.c
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2010      The University of Tennessee and The University
+ *                         of Tennessee Research Foundation.  All rights
+ *                         reserved.
+ *
+ * @precisions normal z -> s d c
+ *
+ */
+#include "dague.h"
+#include <plasma.h>
+#include "data_dist/sparse-matrix/pastix_internal/pastix_internal.h"
+#include "dsparse.h"
+
+#include "zpotrf_sp1dplus.h"
+
+dague_object_t* 
+dsparse_zpotrf_sp_New(sparse_matrix_desc_t *A)
+{
+    dague_zpotrf_sp1dplus_object_t *dague_zpotrf_sp = NULL;
+ 
+    dague_zpotrf_sp = dague_zpotrf_sp1dplus_new(A, (dague_ddesc_t *)A );
+
+    /* dsparse_add2arena_tile(((dague_zpotrf_Url_object_t*)dague_zpotrf)->arenas[DAGUE_zpotrf_Url_DEFAULT_ARENA],  */
+    /*                        A->mb*A->nb*sizeof(Dague_Complex64_t), */
+    /*                        DAGUE_ARENA_ALIGNMENT_SSE, */
+    /*                        MPI_DOUBLE_COMPLEX, A->mb); */
+    
+    return (dague_object_t*)dague_zpotrf_sp;
+}
+ 
+void
+dsparse_zpotrf_sp_Destruct( dague_object_t *o )
+{
+    dague_zpotrf_sp1dplus_object_t *dague_zpotrf_sp = NULL;
+    dague_zpotrf_sp = (dague_zpotrf_sp1dplus_object_t *)o;
+
+    /*dsparse_datatype_undefine_type( &(opotrf->arenas[DAGUE_zpotrf_Url_DEFAULT_ARENA]->opaque_dtt) );*/
+
+    dague_zpotrf_sp1dplus_destroy( dague_zpotrf_sp );
+}
+
+int dsparse_zpotrf_sp( dague_context_t *dague, sparse_matrix_desc_t *A) 
+{
+    dague_object_t *dague_zpotrf_sp = NULL;
+    int info = 0;
+
+    dague_zpotrf_sp = dsparse_zpotrf_sp_New( A );
+
+    if ( dague_zpotrf_sp != NULL )
+    {
+        dague_enqueue( dague, (dague_object_t*)dague_zpotrf_sp);
+        dague_progress( dague );
+        dsparse_zpotrf_sp_Destruct( dague_zpotrf_sp );
+    }
+    return info;
+}
+
