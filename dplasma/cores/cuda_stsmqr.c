239dc8c0d3eadf2fed4d4c0c7c6fa0744938e5f8
XL <devnull@localhost>
2012-01-26 16:06:20 -0500

    FIX memory_pool include

--- dplasma/cores/cuda_stsmqr.c
+++ dplasma/cores/cuda_stsmqr.c
@@ -28,7 +28,7 @@ int *gpu_load;
 const uint32_t MAX_QUEUE = 80;
 #endif
 #include "data_dist/matrix/matrix.h"
-#include "dplasma/lib/memory_pool.h"
+#include "memory_pool.h"
 
 static void compute_best_unit( uint64_t length, float* updated_value, char** best_unit );
 
