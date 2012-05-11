cf1b4cd458522c5ac18be5bff7e742af0ea553b7
Mathieu Faverge <mfaverge@eecs.utk.edu>
2012-05-11 16:35:33 -0400

    Add crapy sh script to remove single node from sparse dags

---
+++ tools/gen-sparsedag.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+username=`whoami`
+
+cp $1.dot $1-backup.dot
+sed -i '/CSC2CBLK/d' $1.dot
+
+max=`grep "^GEMM_[0-9]* \[" $1.dot | sed 's/GEMM_\([0-9]*\) \[.*/\1/' | sort -g | tail -n 1`
+grep "^GEMM_[0-9]* ->" $1.dot | sed 's/\(GEMM_[0-9]* \)->.*/\1/' > /tmp/tmp-$username-sparse.log
+
+echo $max
+
+for i in `seq 0 $max`
+do 
+    res=`grep "GEMM_$i " /tmp/tmp-$username-sparse.log`
+    #echo "Resultat is x${res}x"
+    if [ -z "$res" ]
+    then
+        echo -n "\r$i"
+        sed -i "/GEMM_$i /d" $1.dot
+    fi
+done
+echo "\n"
+
+rm -f /tmp/tmp-$username-sparse.log
+wc -l $1.dot
