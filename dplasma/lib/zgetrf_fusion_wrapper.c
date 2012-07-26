bdbc6fd225f7504f51715edaa310186fa133a1f1
Mathieu Faverge <mfaverge@eecs.utk.edu>
2012-07-26 15:58:00 -0400

    Remove the V workspace and put only one instance of V as suggested by Omar

--- dplasma/lib/zgetrf_fusion_wrapper.c
+++ dplasma/lib/zgetrf_fusion_wrapper.c
@@ -28,7 +28,7 @@ dague_object_t* dplasma_zgetrf_fusion_New( tiled_matrix_desc_t *A,
                                            int *info )
 {
     dague_object_t *dague_zgetrf_fusion = NULL;
-    two_dim_block_cyclic_t *UMAT, *V, *BUFFER;
+    two_dim_block_cyclic_t *UMAT, *BUFFER;
     int mb = A->mb, nb = A->nb;
 
     /* The code has to be fixed for N >> M */
@@ -46,18 +46,6 @@ dague_object_t* dplasma_zgetrf_fusion_New( tiled_matrix_desc_t *A,
          IB*P, nb*Q,      /* Dimensions of the submatrix          */
          1, 1, P));
 
-
-    V = (two_dim_block_cyclic_t*)malloc(sizeof(two_dim_block_cyclic_t));
-    PASTE_CODE_INIT_AND_ALLOCATE_MATRIX(
-        (*V), two_dim_block_cyclic,
-        (V, matrix_ComplexDouble, matrix_Tile,
-         A->super.nodes, A->super.cores, A->super.myrank,
-         LDV,     nb,      /* Dimensions of the tile               */
-         2*LDV*P, nb*Q,    /* Dimensions of the matrix             */
-         0,       0,       /* Starting points (not important here) */
-         2*LDV*P, nb*Q,    /* Dimensions of the submatrix          */
-         1, 1, P));
-
     BUFFER = (two_dim_block_cyclic_t*)malloc(sizeof(two_dim_block_cyclic_t));
     PASTE_CODE_INIT_AND_ALLOCATE_MATRIX(
         (*BUFFER), two_dim_block_cyclic,
@@ -73,7 +61,6 @@ dague_object_t* dplasma_zgetrf_fusion_New( tiled_matrix_desc_t *A,
     dague_zgetrf_fusion = (dague_object_t*)dague_zgetrf_fusion_new((dague_ddesc_t*)A,
                                                                    (dague_ddesc_t*)IPIV,
                                                                    (dague_ddesc_t*)UMAT,
-                                                                   (dague_ddesc_t*)V,
                                                                    (dague_ddesc_t*)BUFFER,
                                                                    IB,
                                                                    P,
@@ -129,11 +116,6 @@ dplasma_zgetrf_fusion_Destruct( dague_object_t *o )
     dague_data_free( desc->mat );
     dague_ddesc_destroy( dague_zgetrf_fusion->UMAT );
     free( dague_zgetrf_fusion->UMAT );
-
-    desc = (two_dim_block_cyclic_t*)(dague_zgetrf_fusion->V);
-    dague_data_free( desc->mat );
-    dague_ddesc_destroy( dague_zgetrf_fusion->V );
-    free( dague_zgetrf_fusion->V );
 
     desc = (two_dim_block_cyclic_t*)(dague_zgetrf_fusion->BUFFER);
     dague_data_free( desc->mat );
