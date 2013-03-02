c62e33b0af631dc5b204841c4bc70ae4ef5ee3b4
Wei Wu <wwu12@utk.edu>
2013-03-01 23:08:24 -0500

    sever now uses main thread to accept task and put task in task queue, one or more threads will execute tasks in task queue

--- src/devices/mic/dague_mic_server/dague_mic_server.c
+++ src/devices/mic/dague_mic_server/dague_mic_server.c
@@ -4,14 +4,40 @@
 #include <mkl.h>
 #include "dague_mic_server.h"
 
+#define MIC_TASK_NUM_OF_THREADS    4
+
 scif_epd_t epd;
 scif_epd_t mic_epd;
 
 mic_command_function_t mic_command_array[COMMAND_FUN_MAX];
+mic_task_function_t mic_task_array[COMMAND_FUN_MAX];
+mic_task_queue_t *mic_server_task_queue;
 
-int mic_send_sync(scif_epd_t epd, void *msg, int len) {
+int mic_send_sync(scif_epd_t mepd, void *msg, int len) {
 	int err;
-	if ((err = scif_send(epd, msg, len, 1)) <= 0) {
+	if ((err = scif_send(mepd, msg, len, 1)) <= 0) {
+		err = errno;
+		printf("[Error] scif_send failed with err %d\n", errno);
+		fflush(stdout);
+		return ERROR;
+	}
+	return SUCCESS;
+}
+
+int mic_send_async(scif_epd_t mepd, void *msg, int len) {
+	int err;
+	if ((err = scif_send(mepd, msg, len, 0)) <= 0) {
+		err = errno;
+		printf("[Error] scif_send failed with err %d\n", errno);
+		fflush(stdout);
+		return ERROR;
+	}
+	return SUCCESS;
+}
+
+int mic_recv_sync(scif_epd_t mepd, void *msg, int len) {
+	int err;
+	if ((err = scif_recv(mepd, msg, len, 1)) <= 0) {
 		err = errno;
 		printf("[Error] scif_recv failed with err %d\n", errno);
 		fflush(stdout);
@@ -20,15 +46,15 @@ int mic_send_sync(scif_epd_t epd, void *msg, int len) {
 	return SUCCESS;
 }
 
-int mic_recv_sync(scif_epd_t epd, void *msg, int len) {
+int mic_recv_async(scif_epd_t mepd, void *msg, int len) {
 	int err;
-	if ((err = scif_recv(epd, msg, len, 1)) <= 0) {
+	if ((err = scif_recv(mepd, msg, len, 0)) < 0) {
 		err = errno;
 		printf("[Error] scif_recv failed with err %d\n", errno);
 		fflush(stdout);
-		return ERROR;
+		return err;
 	}
-	return SUCCESS;
+	return err;
 }
 
 int mic_malloc_server(void **addr, size_t nbyte, off_t *offset)
@@ -64,7 +90,7 @@ int mic_server_init()
 	struct scif_portID portID;
 	
 	req_port = 2055;
-	req_queue_len = 5;
+	req_queue_len = 25;
 
 	printf("[Log] Server starting...\n");
 
@@ -108,6 +134,28 @@ int mic_server_close()
 
 }
 
+int mic_server_task_enqueue(mic_task_context_t * this_task)
+{
+    pthread_mutex_lock(&(mic_server_task_queue->task_queue_lock));
+    if (mic_server_task_queue->total_task == 0) {
+        mic_server_task_queue->head = this_task;
+        mic_server_task_queue->execution = this_task;
+    } else if (mic_server_task_queue->runable_task == 0) {
+        mic_server_task_queue->execution = this_task;
+        mic_server_task_queue->end->next_task = this_task;
+    } else {
+        mic_server_task_queue->end->next_task = this_task;
+    }
+    mic_server_task_queue->end = this_task;
+    mic_server_task_queue->total_task ++;
+    mic_server_task_queue->runable_task ++;
+    this_task->task_status = TASK_READY;
+    printf("[Log] task %p enqueued, id %d, total task: %d, runable task: %d\n", this_task, this_task->task_id, mic_server_task_queue->total_task, mic_server_task_queue->runable_task);
+    pthread_mutex_unlock(&(mic_server_task_queue->task_queue_lock));
+
+    return SUCCESS;
+}
+
 int mic_command_malloc()
 {
     off_t offset;
@@ -134,13 +182,101 @@ int mic_command_malloc()
     return SUCCESS;
 }
 
-int mic_server_process_command(mic_command_function_t fp)
+int mic_command_dgemm()
 {
-    fp();
+    mic_task_context_t *this_task;
+
+    printf("[Log] dgemm command accepted\n");
+    mic_gemm_param_t *mic_dgemm_param = (mic_gemm_param_t *)malloc(sizeof(mic_gemm_param_t));
+    if (mic_recv_sync(mic_epd, mic_dgemm_param, sizeof(mic_gemm_param_t)) == ERROR) { 
+        return ERROR;
+    }
+    this_task = (mic_task_context_t *)malloc(sizeof(mic_task_context_t));
+    this_task->task_name = MIC_DGEMM;
+    this_task->task_data = mic_dgemm_param;
+    this_task->task_id = mic_dgemm_param->task_id;
+    this_task->next_task = NULL;
+
+    mic_server_task_enqueue(this_task);
+
+    return SUCCESS;
+	
+}
+
+int mic_command_sgemm()
+{
+    mic_task_context_t *this_task;
+
+    printf("[Log] sgemm command accepted\n");
+    mic_gemm_param_t *mic_sgemm_param = (mic_gemm_param_t *)malloc(sizeof(mic_gemm_param_t));
+    if (mic_recv_sync(mic_epd, mic_sgemm_param, sizeof(mic_gemm_param_t)) == ERROR) { 
+        return ERROR;
+    }
+    this_task = (mic_task_context_t *)malloc(sizeof(mic_task_context_t));
+    this_task->task_name = MIC_DGEMM;
+    this_task->task_data = mic_sgemm_param;
+    this_task->task_id = mic_sgemm_param->task_id;
+    this_task->next_task = NULL;
+
+    mic_server_task_enqueue(this_task);
+
+    return SUCCESS;
+	
+}
+
+int mic_command_check_completion()
+{
+    mic_task_context_t *this_task;
+    int ack = -1;
+    
+  //  printf("[Log] check completion accepted\n");
+    this_task = mic_server_task_queue->head;
+    if (this_task != NULL) {
+    //    printf("i am herer\n");
+        if (this_task->task_status == TASK_FINISH) {
+            pthread_mutex_lock(&(mic_server_task_queue->task_queue_lock));
+            mic_server_task_queue->head = this_task->next_task;
+            mic_server_task_queue->total_task --;
+            pthread_mutex_unlock(&(mic_server_task_queue->task_queue_lock));
+            ack = 1;
+            if (mic_send_sync(mic_epd, &ack, sizeof(ack)) == ERROR) {
+                return ERROR;
+            }
+            if (this_task->task_data != NULL) {
+                free(this_task->task_data);
+            }
+            ack = this_task->task_id;
+            printf("I send task %d ACK back, this %p, next %p\n", ack, this_task, this_task->next_task);
+            free(this_task);
+            return SUCCESS;
+        } else {
+            ack = 0;
+            if (mic_send_sync(mic_epd, &ack, sizeof(ack)) == ERROR) {
+                return ERROR;
+            }
+        }
+    }
+    ack = 2;     // task queue is empty
+    if (mic_send_sync(mic_epd, &ack, sizeof(ack)) == ERROR) {
+        return ERROR;
+    }
+    
     return SUCCESS;
 }
 
-int process_task()
+int mic_task_dgemm(mic_task_context_t *this_task)
+{
+ //   sleep(1);
+	return SUCCESS;
+}
+
+int mic_task_sgemm(mic_task_context_t *this_task)
+{
+ //   sleep(0.02);
+	return SUCCESS;
+}
+
+int mic_server_accept_command()
 {
 	int command, run_flag;
 	run_flag = 1;
@@ -149,30 +285,127 @@ int process_task()
 		if (mic_recv_sync(mic_epd, &command, sizeof(command)) == ERROR) { 
 			return ERROR;
 		}
-        
-		if (command >= COMMAND_FUN_MAX || mic_command_array[command] == NULL) {
+        if (command >= COMMAND_FUN_MAX || mic_command_array[command] == NULL) {
             printf("unknow command %d\n", command);
         } else if (command == CLOSE_MIC) {
             run_flag = 0;
         } else {
-            mic_server_process_command(mic_command_array[command]);
+            (mic_command_array[command])();
         }
-	}
-
-	
+    }
 
 	return SUCCESS;
 
 }
 
-int mic_command_array_init()
+void *mic_server_progress_task_queue(void *_argv)
+{
+    mic_task_context_t *this_task;
+    int *pid = (int *)_argv;
+    printf("my pid %d\n", *pid);
+
+    while(1) {
+        if (mic_server_task_queue->runable_task > 0) {
+            pthread_mutex_lock(&(mic_server_task_queue->task_queue_lock));
+            this_task = mic_server_task_queue->execution;
+            if (this_task != NULL) {
+                if (this_task->task_status == TASK_READY) {
+                    mic_server_task_queue->execution = this_task->next_task;
+			        mic_server_task_queue->runable_task --;
+                    printf("thread %d grab task id %d, runable task left %d, next task %p\n", *pid, this_task->task_id, mic_server_task_queue->runable_task, this_task->next_task);
+                }
+            }
+            pthread_mutex_unlock(&(mic_server_task_queue->task_queue_lock));
+
+            if (this_task != NULL) {
+                (mic_task_array[this_task->task_name])(this_task);
+                this_task->task_status = TASK_FINISH;
+              //  printf("I finished task %d\n", this_task->task_id);
+            }
+        }
+        
+        
+    }
+}
+
+void *mic_server_finish_task_queue()
+{
+    mic_task_context_t *this_task;
+    int ack = -1;
+
+    while (1) {
+        this_task = mic_server_task_queue->head;
+        if (this_task != NULL) {
+            if (this_task->task_status == TASK_FINISH) {
+                pthread_mutex_lock(&(mic_server_task_queue->task_queue_lock));
+                mic_server_task_queue->head = this_task->next_task;
+                mic_server_task_queue->total_task --;
+                pthread_mutex_unlock(&(mic_server_task_queue->task_queue_lock));
+                if (this_task->task_data != NULL) {
+                    free(this_task->task_data);
+                }
+                ack = this_task->task_id;
+               // if (mic_send_async(mic_epd, &ack, sizeof(ack)) == ERROR) { 
+			     //   return ERROR;
+		        //}
+                free(this_task);
+                printf("I send task %d ACK back\n", ack);
+            }
+        }
+    }
+}
+
+int mic_server_run()
+{
+    pthread_t threads[MIC_TASK_NUM_OF_THREADS];
+    pthread_t thread_send_ack;
+    int rc, i;
+    
+    for (i = 0; i < MIC_TASK_NUM_OF_THREADS; i++) {
+        int *pid = (int *)malloc(sizeof(int));
+        *pid = i;
+        rc = pthread_create(&threads[i], NULL, mic_server_progress_task_queue, pid);
+        if (rc) {
+            printf("[Error] can not create thread %d\n", i);
+        }
+    }
+
+  /*  rc = pthread_create(&thread_send_ack, NULL, mic_server_finish_task_queue, NULL);
+    if (rc) {
+        printf("[Error] can not create finsh task thread\n");
+    }*/
+
+    mic_server_accept_command();
+    return SUCCESS;
+}
+
+int mic_task_queue_init()
 {
     int i;
     for (i = 0; i < COMMAND_FUN_MAX; i++) {
         mic_command_array[i] = NULL;
     }
     mic_command_array[MALLOC_MEM] = mic_command_malloc;
-  //  mic_command_array[MIC_DGEMM] = mic_command_dgemm;
+    mic_command_array[CLOSE_MIC] = mic_server_close;
+    mic_command_array[MIC_DGEMM] = mic_command_dgemm;
+    mic_command_array[MIC_SGEMM] = mic_command_sgemm;
+    mic_command_array[CHECK_COMPLETION] = mic_command_check_completion;
+
+    mic_task_array[MIC_DGEMM] = mic_task_dgemm;
+    mic_task_array[MIC_SGEMM] = mic_task_sgemm;
+    
+    mic_server_task_queue = (mic_task_queue_t *)malloc(sizeof(mic_task_queue_t));
+	mic_server_task_queue->head = NULL;
+    mic_server_task_queue->end = NULL;
+    mic_server_task_queue->execution = NULL;
+    mic_server_task_queue->total_task = 0;
+    mic_server_task_queue->runable_task = 0;
+    mic_server_task_queue->finished_task = 0;
+	
+	pthread_mutex_init(&(mic_server_task_queue->task_queue_lock), NULL);
+    pthread_cond_init(&(mic_server_task_queue->task_ready), NULL);
+
+    
     return SUCCESS;
 }
 
@@ -182,8 +415,8 @@ int main(int argc, char *argv[])
 		printf("[Error]:server can not start\n");
 		exit(1);
 	}
-    mic_command_array_init();
-	process_task();	
+    mic_task_queue_init();
+	mic_server_run();	
 
 	if (mic_server_close() != SUCCESS) {
 		printf("[Error]:server can not close\n");
--- src/devices/mic/dague_mic_server/dague_mic_server.h
+++ src/devices/mic/dague_mic_server/dague_mic_server.h
@@ -2,6 +2,7 @@
 #define DAGUE_MIC_SERVER_H
 
 #include <scif.h>
+#include <pthread.h>
 
 #define SUCCESS			1
 #define ERROR			0
@@ -13,7 +14,13 @@
 #define MALLOC_MEM			10
 #define CLOSE_MIC			11
 #define SYNC_WRITE			12
+#define CHECK_COMPLETION    13
 #define MIC_DGEMM			20
+#define MIC_SGEMM			21
+
+#define TASK_READY			101
+#define TASK_EXECUTION		102
+#define TASK_FINISH			103
 
 enum micMemcpyKind {
 	micMemcpyHostToHost,
@@ -28,33 +35,70 @@ typedef struct mic_mem_struct {
 	size_t actual_nbyte;
 }mic_mem_t;
 
+typedef struct mic_task_context {
+    void *task_data;                         /** data of the task */
+    int task_status;                         /** status of the task, TASK_READY, TASK_FINISH */
+    struct mic_task_context *next_task;
+    pthread_mutex_t task_lock;               /** lock the task when a thread is trying to take it */
+	int task_name;                           /** name of task */
+    int task_id;                             /** client uses it to identify task */
+}mic_task_context_t;
+
+typedef struct mic_task_queue {
+    pthread_mutex_t task_queue_lock;         /** lock the queue when a thread is trying to take a task from task queue */
+    pthread_cond_t task_ready;               /** cond_t for job ready */
+    struct mic_task_context *head;           /** head of task queue, IN */ 
+    struct mic_task_context *end;            /** end of task queue, OUT */
+    struct mic_task_context *execution;      /** this task will be executed next */
+    int total_task;                          /** number of total tasks in queue */
+    int runable_task;                        /** number of tasks could be executed */
+    int finished_task;
+}mic_task_queue_t;
+
 typedef struct mic_gemm_param {
-	mic_mem_t gemm_A;
-	mic_mem_t gemm_B;
-	mic_mem_t gemm_C;
+	void* gemm_A;
+	void* gemm_B;
+	void* gemm_C;
 	int m;
 	int n;
 	int k;
+    int task_id;                              
 }mic_gemm_param_t;
 
 typedef int micEvent_t;
 
 typedef int (*mic_command_function_t)();
 
+typedef int (*mic_task_function_t)(mic_task_context_t *this_task);
+
 /** send and receive command to/from client */
-int mic_send_sync(scif_epd_t epd, void *msg, int len);
-int mic_recv_sync(scif_epd_t epd, void *msg, int len);
+int mic_send_sync(scif_epd_t mepd, void *msg, int len);
+int mic_send_async(scif_epd_t mepd, void *msg, int len);
+int mic_recv_sync(scif_epd_t mepd, void *msg, int len);
 
 /** malloc mem in mic */
 int mic_malloc_server(void **addr, size_t nbyte, off_t *offset);
 
-/** process command from client */
+/** accept command from client */
 int mic_command_array_init();
-int mic_server_process_command(mic_command_function_t fp);
+int mic_server_accept_command();
+int mic_server_task_enqueue(mic_task_context_t * this_task);
 int mic_command_malloc();
+int mic_command_dgemm();
+int mic_command_sgemm();
+int mic_command_check_completion();
+
+/** process task in queue */
+void *mic_server_progress_task_queue(void *_argv);
+int mic_task_dgemm(mic_task_context_t * this_task);
+int mic_task_sgemm(mic_task_context_t * this_task);
+
+/** finish task in queue, send comfirmation back to client */
+void *mic_server_finish_task_queue();
 
 /** init and close server */
 int mic_server_init();
 int mic_server_close();
+int mic_server_run();
 
 #endif
